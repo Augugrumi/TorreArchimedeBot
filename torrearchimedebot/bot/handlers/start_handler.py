34fc60fc87817ab251c81fc5c3ba7002592a388b
Davide Polonio <poloniodavide@gmail.com>
2017-12-08 13:37:32 +0100

    Add first handler. Still to see if it works

---
+++ torrearchimedebot/bot/handlers/start_handler.py
@@ -0,0 +1,6 @@
+from .abs_handler import AbsHandler
+
+class StartHandler(AbsHandler):
+
+    def handleMessage(self):
+        return "Test"
--- torrearchimedebot/bot/telegram_view.py
+++ torrearchimedebot/bot/telegram_view.py
@@ -1,7 +1,17 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from handlers.start_handler import StartHandler
 import os
 
 class TelegramView:
-    updater = Updater(token=os.environ['DEBUSSY'])
-    dispatcher = updater.dispatcher
+
+    def __init__(self):
+        self._updater = Updater(token=os.environ['TG_TOKEN'])
+        self._dispatcher = self._updater.dispatcher
+        // Start Handler
+        start_handler = CommandHandler('start', self.start)
+        self._dispatcher.add_handler('start', start_handler)
+
+    def start(self, bot, update) :
+        handler = StartHandler()
+        bot.send_message(chat_id=update.message.chat_id, text=handler.handleMessage())
