eefec626a46561164361de78ad0f0194e4a0ad4b
mzanella <zanna0150@gmail.com>
2017-12-08 11:01:00 +0100

    Add HTML page parsing and put results to a class

--- parsing.py
+++ parsing.py
@@ -1,44 +1,68 @@
 import requests
 from lxml import html
 import sys
+import json
 
 class Results:
-    def __init__(self, time, activity, professor, activityType):
-        [timeStart, timeEnd] = time.split(' - ')
-        self.timeStart = timeStart
-        self.timeEnd = timeEnd
+    timeStart = ''
+    timeEnd = ''
+    activity = ''
+    professor = ''
+    activityType = ''
+    
+    def __init__(self, time = '', activity = '', professor = '', activityType = ''):
+        if (time != '') :
+            [timeStart, timeEnd] = time.split(' - ')
+            self.timeStart = timeStart
+            self.timeEnd = timeEnd
+        else :
+            self.timeStart = ''
+            self.timeEnd = ''
         self.activity = activity
         self.professor = professor
         self.activityType = activityType
-   
-    def __init__(self, time, activity, professor):
-        [timeStart, timeEnd] = time.split(' - ')
-        self.timeStart = timeStart
-        self.timeEnd = timeEnd
-        self.activity = activity
-        self.professor = professor
-        self.activityType = ''
     
-    
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+    def isEmpty(self):
+        return (self.timeStart == '' and
+                self.timeEnd == '' and
+                self.activity == '' and
+                self.professor == '' and
+                self.activity == '')
 
 url = 'http://wss.math.unipd.it/display/Pages/1A150.php'
-if (sys.argv[1] != "") :
+if (len(sys.argv) >= 2) :
     url = sys.argv[1]
 f = requests.get(url)
 
-print(f.text)
+#print(f.text)
 
+xpathToTableRows = '//html/body/table/tbody/tr'
+xpathToCellContent = './td/h2/text()'
+xpathToActivityType = './td/h3/text()'
+xpathIfNoLessons = "./td[@class='noevent']/text()"
 tree = html.fromstring(f.text)
-rows = tree.xpath('//html/body/table/tbody/tr') #update your table XPath here
+rows = tree.xpath(xpathToTableRows) #update your table XPath here
 records = []
+cells = ''
 for row in rows:
-    cells = [c for c in row.xpath('./td/h2/text()') if c.strip()]
-    cells += [c for c in row.xpath('./td/h3/text()') if c.strip()]
-    if (len(cells)>0):
-        if (len(cells)==3):
+    cells = [c for c in row.xpath(xpathIfNoLessons) if c.strip()]
+    if (len(cells) == 0) :
+        cells = [c for c in row.xpath(xpathToCellContent) if c.strip()]
+        cells += [c for c in row.xpath(xpathToActivityType) if c.strip()]
+        if (len(cells)<=0):
+            records.append(Results())
+            print(Results)
+        elif (len(cells)==3):
+            records.append(Results(cells[0], cells[1], cells[2]))
             print(Results(cells[0], cells[1], cells[2]))
-        if (len(cells)==4):
+        elif (len(cells)==4):
+            records.append(Results(cells[0], cells[1], cells[2], cells[3]))
             print(Results(cells[0], cells[1], cells[2], cells[3]))
+    else :
+        records.append(Results())
 
-    else:
-        print(Results())
+for r in records:
+    print(r)
