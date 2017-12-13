b1d73e6062ed03c5fa305259c4d29e51d4b6f596
Davide Polonio <poloniodavide@gmail.com>
2017-12-13 10:37:05 +0100

    Add common operation & ignore case for room

--- torrearchimedebot/bot/telegram_controller.py
+++ torrearchimedebot/bot/telegram_controller.py
@@ -6,6 +6,7 @@ from bot.handlers.now_handler import NowHandler
 from bot.handlers.free_handler import FreeHandler
 from bot.handlers.info_handler import InfoHandler
 from .handlers.utility import *
+import logging
 import os
 
 class TelegramController:
@@ -36,26 +37,40 @@ class TelegramController:
             handler = CommandHandler(room, self.roomSchedule)
             self._dispatcher.add_handler(handler)
 
+    def commonOperation(self, bot, update):
+        logging.getLogger().info("Received message from " + \
+        update.message.chat_id + " with text: " + update.message.text)
+        bot.send_chat_action(chat_id=update.message.chat.id, action=telegram.ChatAction.TYPING)
 
     def receiveMessages(self):
         self._updater.start_polling()
 
     def start(self, bot, update):
+        commonOperation(bot, update)
         handler = StartHandler()
         bot.send_message(chat_id=update.message.chat_id, text=handler.handleMessage())
 
     def roomSchedule(self, bot, update):
-        handler = RoomHandler(update.message.text[1:])
+        commonOperation(bot, update)
+        roomId = ""
+        messageRoom = update.message.text[1:]
+        for r in retrieve_rooms():
+            if r.upper() === messageRoom.upper():
+                roomId = r
+        handler = RoomHandler(r)
         bot.send_message(parse_mode='Markdown', chat_id=update.message.chat_id, text=handler.handleMessage())
 
     def now(self, bot, update):
+        commonOperation(bot, update)
         handler = NowHandler()
         bot.send_message(parse_mode='Markdown', chat_id=update.message.chat_id, text=handler.handleMessage())
 
     def free(self, bot, update):
+        commonOperation(bot, update)
         handler = FreeHandler()
         bot.send_message(chat_id=update.message.chat_id, text=handler.handleMessage())
 
     def info(self, bot, update):
+        commonOperation(bot, update)
         handler = InfoHandler()
         bot.send_message(parse_mode='Markdown', chat_id=update.message.chat_id, text=handler.handleMessage())
