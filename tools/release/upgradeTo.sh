d63a34c1920c780eb86b95799b7bb0fde3769a88
Davide Polonio <poloniodavide@gmail.com>
2017-12-14 10:09:40 +0100

    Add parameters

--- tools/release/upgradeTo.sh
+++ tools/release/upgradeTo.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Upgrading tool for TorreArchimedeBot"
-ssh -i $KEY_NAME $USER_TO_SSH@$ADDRESS_TO_SSH "$TRAVIS_TAG"
+ssh -i $KEY_NAME -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$TRAVIS_TAG"
 
 echo "Update scheduled"
