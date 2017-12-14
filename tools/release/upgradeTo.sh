57af0af928e384277d0468d1e04983c65f6517be
Davide Polonio <poloniodavide@gmail.com>
2017-12-14 22:32:26 +0100

    Hide ssh output

--- tools/release/upgradeTo.sh
+++ tools/release/upgradeTo.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo "Upgrading tool for TorreArchimedeBot"
-ssh -i $KEY_NAME -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1"
-
+echo "TorreArchimedeBot upgrading tool"
+ssh -i $KEY_NAME -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1" &>/dev/null
 echo "Update scheduled"
