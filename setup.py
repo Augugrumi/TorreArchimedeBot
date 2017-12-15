66853017f22d1678b8455935e379fc158e9d0eb4
Davide Polonio <poloniodavide@gmail.com>
2017-12-15 09:32:20 +0100

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='torrearchimedebot',
-   version='0.1.3',
+   version='0.2.0',
    description='A useful bot for Torre Archimede schedule',
    license="GPL3",
    long_description=long_description,
