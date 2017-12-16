53ce18afb9e6fa3d0840f97c91ac4a36667ce7af
Davide Polonio <poloniodavide@gmail.com>
2017-12-16 10:54:43 +0100

    Fix key permission

--- tools/release/upgradeTo.sh
+++ tools/release/upgradeTo.sh
@@ -4,7 +4,9 @@ set -e
 
 echo "TorreArchimedeBot upgrading tool"
 echo "Running in `pwd`"
-echo "Listing tools/release"
-ls -l tools/release
+echo "Fixing permissions"
+chmod 600 tools/release/ci_key
+echo "Running ssh"
 ssh -i tools/release/ci_key -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1"
 echo "Update scheduled"
+rm tools/release/ci_key
