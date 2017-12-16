6b6bb5579996babb08d105df5b65a52c34c5c62a
Davide Polonio <poloniodavide@gmail.com>
2017-12-16 10:46:35 +0100

    Add some debug info

--- tools/release/upgradeTo.sh
+++ tools/release/upgradeTo.sh
@@ -4,5 +4,7 @@ set -e
 
 echo "TorreArchimedeBot upgrading tool"
 echo "Running in `pwd`"
-ssh -i tools/release/$KEY_NAME -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1"
+echo "Listing tools/release"
+ls -l tools/release
+ssh -i tools/release/ci_key -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1"
 echo "Update scheduled"
