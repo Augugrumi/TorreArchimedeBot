bbbdcec1c0e858e4a506bacbe0f1c1a94030f530
mzanella <zanna0150@gmail.com>
2017-12-19 21:29:35 +0100

    Change formatting

--- torrearchimedebot/bot/handlers/info_handler.py
+++ torrearchimedebot/bot/handlers/info_handler.py
@@ -1,5 +1,6 @@
 from .abs_handler import AbsHandler
 
+
 class InfoHandler(AbsHandler):
 
     def __init__(self):
@@ -11,8 +12,8 @@ class InfoHandler(AbsHandler):
     def handleMessage(self):
 
         return "*Torre Archimede Bot*, version `" + self.version + \
-    "`.\n_Brought to you with ❤️ by " + self.author + "_.\n" + \
-    "Do you want to contribute or report an issue? You can find us at: " + \
-    self.github + "!\n\nEveryone knows that programmers are machines able to " + \
-    "transform coffee into code. Please help us buying some! \n" + \
-    self.url_donation
+            "`.\n_Brought to you with ❤️ by " + self.author + "_.\n" + \
+            "Do you want to contribute or report an issue? You can find us" + \
+            " at: " + self.github + "!\n\nEveryone knows that programmers " + \
+            "are machines able to transform coffee into code. Please " + \
+            "help us buying some! \n" + self.url_donation
