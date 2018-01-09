f207ce5ec1835e782c831e036ce449a218c6f76c
Davide Polonio <poloniodavide@gmail.com>
2018-01-09 17:35:23 +0100

    Update release script

--- tools/release/upgradeTo.sh
+++ tools/release/upgradeTo.sh
@@ -7,6 +7,6 @@ echo "Running in `pwd`"
 echo "Fixing permissions"
 chmod 600 tools/release/ci_key
 echo "Running ssh"
-ssh -i tools/release/ci_key -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "$1"
+ssh -i tools/release/ci_key -oStrictHostKeyChecking=no $USER_TO_SSH@$ADDRESS_TO_SSH "./update.sh $1"
 echo "Update scheduled"
 rm tools/release/ci_key
